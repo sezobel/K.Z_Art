26387fc9e256182f8dcafb1d3e32007bec554bb2
Samuel Z <samuelezobel@gmail.com>
2015-08-27 13:57:38 -0400

    updated models for listing and users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -17,6 +17,9 @@ class Listing < ActiveRecord::Base
 
 	belongs_to :user
 
+	# validates :name, :description, :price, presence:true 
+	# validates :price, numericality: { greater_than :0 }
+	  validates_attachment_presence :image 
 end 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
 
    has_many :listings
 
+   # validates :name, presence:true
+
 end
